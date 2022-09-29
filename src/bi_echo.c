3f581448b3c60b438f47d75bc3b68277b8c1d114
Bertrand SCHOEFFLER <bschoeff@e2r7p7.clusters.42paris.fr>
2022-09-29 14:51:09 +0200

    working  on echo, pulling structures from  main

--- src/bi_echo.c
+++ src/bi_echo.c
@@ -6,24 +6,45 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 11:20:35 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/09/29 12:23:24 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/09/29 13:13:21 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static 
+static int	is_arg(char *str);
+{
+	int		i;
+	char	*ref;
+
+	ref = "-n";
+	i = -1;
+	while (str[++i])
+		if (str[i] != ref[i])
+			return (0);
+	return (1);
+}
 
 int	bi_echo(char **args)
 {
 	int	i;
-	int	j;
 	int	len;
 	int	n_line;
 
 	i = -1;
 	n_line = 0;
+	if (is_arg(args[0]))
+		n_line = 1;
 	while (args[++i])
-		if (is_arg(args[i]))
-			n_line = 1;
+	{
+		len = 0;
+		while (args[i][len])
+			len++;
+		write(1, args[i], len);
+		if (args[i + 1])
+			write(1, " ", 1);
+	}
+	if (n_line)
+		write(1, "\n", 1);
+	return (0);
 }
--- src/utils_split.c
+++ src/utils_split.c
@@ -1,23 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils_split.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/26 10:16:50 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/09/29 14:43:45 by bschoeff         ###   ########.fr       */
+/*   Created: 2022/09/29 13:15:26 by bschoeff          #+#    #+#             */
+/*   Updated: 2022/09/29 13:15:47 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	main(int ac, char **av, char **envp)
-{
-	(void)ac;
-	(void)av;
-	char **args;
-	bi_pwd();
-	bi_env(envp);
-	return (0);
-}
