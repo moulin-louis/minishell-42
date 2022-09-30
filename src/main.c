6f8560312bfd1f4835f4f46ed3f72aab6d250634
Bertrand SCHOEFFLER <bschoeff@e1r1p7.clusters.42paris.fr>
2022-09-30 16:15:42 +0200

    we got a winner, or maybe we got a wiener...

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 10:16:50 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/09/30 13:12:10 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/09/30 16:13:56 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,18 @@
 int	main(int ac, char **av, char **env)
 {
 	char	**array;
+	int		i;
 
 	(void)ac;
 	(void)av;
 	(void)env;
-	array = ft_split("  Je ne sais    pas  ");
+	array = NULL;
+	array = ft_split("  je peux        pa  ");
+	i = -1;
+	while (array[++i])
+		printf("array[%i]: %s\n", i, array[i]);
+	while (--i >= 0)
+		free(array[i]);
 	free(array);
 	return (0);
 }
--- src/ut_split.c
+++ src/ut_split.c
@@ -6,18 +6,35 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 13:15:26 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/09/30 13:23:30 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/09/30 16:14:38 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-static int	split_da_shit(char **arr, char *str)
+static void	clean_split(char **arr)
 {
-	int	len;
 	int	i;
 
-	return (1);
+	i = 0;
+	while (arr[i])
+		i++;
+	i--;
+	while (i >= 0)
+		free(arr[i]);
+	free(arr);
+}
+
+static int	word_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != ' ')
+		i++;
+	return (i);
 }
 
 static int	word_count(char *str)
@@ -28,32 +45,63 @@ static int	word_count(char *str)
 	count = 0;
 	if (!str || !*str)
 		return (count);
-	i = -1;
-	while (str[++i])
+	i = 0;
+	while (str[i])
 	{
-		if (str[i] != ' ')
+		while (str[i] != '\0' && str[i] == ' ')
+			i++;
+		if (str[i] && str[i] != ' ')
 		{
 			count++;
-			while (str[i] != ' ')
+			while (str[i] && str[i] != ' ')
 				i++;
 		}
 	}
 	return (count);
 }
 
+static char	*do_the_split(char *s2, int len)
+{
+	int		i;
+	char	*s1;
+
+	s1 = malloc(len + 1);
+	if (!s1)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+
 char	**ft_split(char *str)
 {
 	char	**arr;
 	int		words;
+	int		i;
+	int		len;
 
 	arr = NULL;
 	words = word_count(str);
-	printf("words in ft_split: %i\n", words);
 	arr = malloc(sizeof(char *) * (words + 1));
 	if (!arr)
 		return (perror("Split malloc"), arr);
 	arr[words] = 0;
-	if (split_da_shit(arr, str))
-		return (arr);
-	return (NULL);
+	i = -1;
+	while (++i < words)
+	{
+		if (*str)
+			while (*str == ' ')
+				str++;
+		len = word_len(str);
+		arr[i] = do_the_split(str, len);
+		if (!arr[i])
+			return (clean_split(arr), NULL);
+		str += len;
+	}
+	return (arr);
 }
