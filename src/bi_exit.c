be24075e53fb94f36e605b11820d436814e071ba
Bertrand SCHOEFFLER <bschoeff@e1r10p11.clusters.42paris.fr>
2022-10-07 13:04:06 +0200

    bi_exit: changed order of cmd's assessemtn to avoid invali read. Dealt with proper overflow of status return value

--- src/bi_exit.c
+++ src/bi_exit.c
@@ -6,7 +6,7 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 11:12:28 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/10/07 12:30:59 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/10/07 13:02:36 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,44 @@
 #include <errno.h>
 
 
-static unsigned char	statoc(char *str)
+static unsigned char	statouc(char *str)
 {
 	int				i;
 	unsigned char	n;
+	int				sign;
 
 	i = -1;
 	n = 0;
+	sign = 1;
+	if (str[0] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
 	while (str[++i])
 	{
 		n *= 10;
 		n += str[i] - '0';
 	}
-	printf("%i\n", n);
-	return (n);
+	printf("%i\n", (unsigned char)(n * sign));
+	return (n * sign);
 }
 
 int	bi_exit(t_cati **mini)
 {
 	int	status;
 
-	if ((*mini)->cmd[2])
-	{
-		printf("bash: exit: too many arguments\n");
-		errno = 1;
-		return (1);
-	}
 	status = 0;
 	if ((*mini)->cmd[1])
-		status = statoc((*mini)->cmd[1]);
+	{
+		status = statouc((*mini)->cmd[1]);
+		if ((*mini)->cmd[2])
+		{
+			printf("bash: exit: too many arguments\n");
+			errno = 1;
+			return (clean_mini(mini), 1);
+		}
+	}
 	clean_mini(mini);
 	exit(status);
 }
