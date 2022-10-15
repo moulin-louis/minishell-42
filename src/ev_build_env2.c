2a53f13c87e90cc82d88d5ac7ba35714a0da0446
Bertrand SCHOEFFLER <bschoeff@e1r1p12.clusters.42paris.fr>
2022-10-15 10:34:52 +0200

    handled launch wothout env: set baic env as per bsh

--- src/ev_build_env2.c
+++ src/ev_build_env2.c
@@ -6,7 +6,7 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 14:21:42 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/10/13 14:38:59 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/10/15 10:29:26 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,29 @@
 #include <stdio.h>
 #include <unistd.h>
 
+static char	*ft_getpwd(void)
+{
+	char	*pwd;
+	char	*buff;
+	int		size;
+
+	size = 1;
+	pwd = NULL;
+	buff = NULL;
+	while (!pwd)
+	{
+		pwd = getcwd(buff, size);
+		size++;
+	}
+	free(buff);
+	return (pwd);
+}
+
 int	set_new1(t_envp *new1)
 {
 	new1->var = malloc(3 * sizeof(char *));
 	new1->var[0] = malloc(7);
-	new1->var[0] = "OLDPWD";
+	new1->var[0] = ut_strcpy("OLDPWD");
 	new1->var[1] = NULL;
 	new1->var[2] = NULL;
 	return (1);
@@ -29,9 +47,9 @@ int	set_new2(t_envp *new2)
 {
 	new2->var = malloc(3 * sizeof(char *));
 	new2->var[0] = malloc(4);
-	new2->var[0] = "PWD";
+	new2->var[0] = ut_strcpy("PWD");
 	new2->var[1] = malloc(5);
-	new2->var[1] = "/mnt";
+	new2->var[1] = ft_getpwd();
 	new2->var[2] = NULL;
 	return (1);
 }
@@ -40,9 +58,9 @@ int	set_new3(t_envp *new3)
 {
 	new3->var = malloc(3 * sizeof(char *));
 	new3->var[0] = malloc(6);
-	new3->var[0] = "SHLVL";
+	new3->var[0] = ut_strcpy("SHLVL");
 	new3->var[1] = malloc(2);
-	new3->var[1] = "1";
+	new3->var[1] = ut_strcpy("1");
 	new3->var[2] = NULL;
 	return (1);
 }
