45111b12eb39aaee1592677156a7b6edccd021b3
Bertrand SCHOEFFLER <bschoeff@e1r1p5.clusters.42paris.fr>
2022-10-17 10:22:02 +0200

    added perfect malloc fail management when creating the env

--- src/ev_build_env2.c
+++ src/ev_build_env2.c
@@ -6,7 +6,7 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 14:21:42 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/10/17 08:59:25 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/10/17 10:17:55 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ int	set_new1(t_envp *new1)
 {
 	new1->var = ut_calloc(3, sizeof(char *));
 	new1->var[0] = ut_calloc(7, 1);
+	if (!new1->var[0])
+		return (0);
 	new1->var[0] = ut_strcpy("OLDPWD");
 	new1->var[1] = NULL;
 	new1->var[2] = NULL;
@@ -47,9 +49,10 @@ int	set_new2(t_envp *new2)
 {
 	new2->var = ut_calloc(3, sizeof(char *));
 	new2->var[0] = ut_calloc(4, 1);
-	new2->var[0] = ut_strcpy("PWD");
-	//new2->var[1] = ut_calloc(5, 1);
 	new2->var[1] = ft_getpwd();
+	if (!new2->var[0] || !new2->var[1])
+		return (0);
+	new2->var[0] = ut_strcpy("PWD");
 	new2->var[2] = NULL;
 	return (1);
 }
@@ -58,8 +61,10 @@ int	set_new3(t_envp *new3)
 {
 	new3->var = ut_calloc(3, sizeof(char *));
 	new3->var[0] = ut_calloc(6, 1);
-	new3->var[0] = ut_strcpy("SHLVL");
 	new3->var[1] = ut_calloc(2, 1);
+	if (!new3->var[0] || !new3->var[1])
+		return (0);
+	new3->var[0] = ut_strcpy("SHLVL");
 	new3->var[1] = ut_strcpy("1");
 	new3->var[2] = NULL;
 	return (1);
