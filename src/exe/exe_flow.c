654c696b7071899a8a83b9a50b211504eb97ba78
Bertrand SCHOEFFLER <bschoeff@e1r10p11.clusters.42paris.fr>
2022-10-24 16:44:39 +0200

    Luke, I am your parent, says a man to a fork

--- src/exe/exe_flow.c
+++ src/exe/exe_flow.c
@@ -6,7 +6,7 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 09:30:58 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/10/24 15:53:57 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/10/24 16:37:35 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,31 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
 
-static int	manage_fds(t_cati **mini, t_cati *node)
+static int	manage_fds(t_cati *node)
 {
-	(void)mini;
-	(void)node;
-	return (0);
+	if (node->in_file)
+	{
+		node->fds->in_fd = open(node->infile, O_RDONLY);
+		if (node->fds->in_fd == -1)
+			return (printf("shellnado: %s: %s\n", node->infile, strerror(errno)), 0);
+	}
+	if (node->out_trunc)
+	{
+		node->fds->out_fd = open(node->outfile, O_WRONLY);
+		if (node->fds->out_fd == -1)
+			return (printf("shellnado: %s: %s\n", node->infile, strerror(errno)), 0);
+	}
+	if (node->out_append)
+	{
+		node->fds->out_fd = open(node->outfile, O_APPEND);
+		if (node->fds->out_fd == -1)
+			return (printf("shellnado: %s: %s\n", node->infile, strerror(errno)), 0);
+	}
+	return (1);
 }
 
 static void	set_elements(t_cati **mini, t_cati *node)
@@ -32,7 +51,7 @@ static void	set_elements(t_cati **mini, t_cati *node)
 	}
 	if (!node->next && !node->outfile)
 		close(node->fds->pfd_1[1]);
-	if (node->out_file)
+	if (node->out_append || node->out_trunc)
 		dup2(node->fds->out_fd, 1);
 	if (node->in_pipe)
 		dup2(node->fds->pfd_1[0], 0);
@@ -43,7 +62,7 @@ static void	set_elements(t_cati **mini, t_cati *node)
 int	first_fork(t_cati **mini, t_cati *node)
 {
 	set_elements(mini, node);
-	if (!manage_fds(mini, node))
+	if (!manage_fds(node))
 		return (0);
 	if (node->builtin && !node->out_pipe)
 		return (exe_bi_launcher(mini, node));
