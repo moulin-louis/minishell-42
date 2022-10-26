c80fb17c92cb87278a38b041b290bc8696b02c68
Bertrand SCHOEFFLER <bschoeff@e1r10p11.clusters.42paris.fr>
2022-10-26 15:36:24 +0200

    handled when cmd is a directory

--- src/exe/exe_set_path_cmd.c
+++ src/exe/exe_set_path_cmd.c
@@ -6,7 +6,7 @@
 /*   By: bschoeff <bschoeff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 10:39:44 by bschoeff          #+#    #+#             */
-/*   Updated: 2022/10/26 15:11:51 by bschoeff         ###   ########.fr       */
+/*   Updated: 2022/10/26 15:34:19 by bschoeff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 static int	check_access(char **arr, t_cati **mini, t_cati *node)
 {
@@ -70,7 +72,8 @@ static int	parse_env_path(char **arr, t_cati **mini, t_cati *node)
 
 static int	explicit_path(t_cati **mini, t_cati *node)
 {
-	int	i;
+	int			i;
+	struct stat	buf;
 
 	i = -1;
 	while (node->cmd[0][++i])
@@ -80,6 +83,12 @@ static int	explicit_path(t_cati **mini, t_cati *node)
 			node->path_cmd = ut_strcpy(node->cmd[0]);
 			if (!node->path_cmd)
 				full_exit(mini, 1);
+			stat(node->path_cmd, &buf);
+			if (S_ISDIR(buf.st_mode))
+			{
+				printf("shellnado: %s: Is a directory\n", node->path_cmd);
+				full_exit(mini, 126);
+			}
 			if (access(node->path_cmd, R_OK || X_OK))
 			{
 				printf("shellnado: %s: %s\n", node->cmd[0], strerror(errno));
