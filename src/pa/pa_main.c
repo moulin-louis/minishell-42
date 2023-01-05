7d2989409b8c43f97925fd7c6ac9c62acbf9eaf8
Louis Moulin <louis.moulin@outlook.fr>
2023-01-05 21:26:33 +0100

    add untracked filed and update todo list

--- src/pa/pa_main.c
+++ src/pa/pa_main.c
@@ -6,7 +6,7 @@
 /*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 13:12:30 by loumouli          #+#    #+#             */
-/*   Updated: 2023/01/05 19:01:17 by loumouli         ###   ########.fr       */
+/*   Updated: 2023/01/05 21:25:44 by loumouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include <stdlib.h>
 
 /*TO DO LIST :
-- FIX '<>' -> FIX SPLIT FOR REDIRECTION 
 - FIX CTRL + C DOUBLE SHELL WHEN CAT IS RUNNING
 */
 
---
+++ src/pa/pa_split_token.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pa_split_token.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 19:42:18 by loumouli          #+#    #+#             */
+/*   Updated: 2023/01/05 21:23:35 by loumouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <errno.h>
+#include <stdlib.h>
+
+int	len_next_tok(char *str, int pos)
+{
+	int	result;
+
+	result = 0;
+	if (str[pos] != '|' && str[pos] != '<' && str[pos] != '>')
+	{
+		while (str[pos] && (str[pos] != '|' && str[pos] != '<' && str[pos]
+				!= '>'))
+		{
+			pos++;
+			result++;
+		}
+		return (result);
+	}
+	if (str[pos] == '|' || str[pos] == '>' || str[pos] == '<')
+		return (1);
+	else
+		return (2);
+	return (result);
+}
+
+char	*create_string(char *str, int *pos)
+{
+	int		len;
+	char	*result;
+	int		x;
+
+	x = 0;
+	len = len_next_tok(str, *pos);
+	result = malloc(len + 1);
+	if (!result)
+		return (NULL);
+	result[len] = '\0';
+	while (x < len)
+	{
+		result[x] = str[*pos];
+		(*pos)++;
+		x++;
+	}
+	return (result);
+}
+
+char	**split_tok(char *str, int nbr_tok, t_tok **lst, t_cati **mini)
+{
+	char	**result;
+	int		x;
+	int		pos_string;
+
+	x = 0;
+	pos_string = 0;
+	result = malloc(sizeof(char *) * (nbr_tok + 1));
+	if (!result)
+		ut_clean_parsing_n_quit(mini, lst, errno);
+	result[nbr_tok] = NULL;
+	while (x < nbr_tok)
+	{
+		result[x] = create_string(str, &pos_string);
+		if (!result[x])
+		{
+			clean_split(result);
+			ut_clean_parsing_n_quit(mini, lst, errno);
+		}
+		x++;
+	}
+	return (result);
+}
